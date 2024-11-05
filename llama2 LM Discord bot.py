bfea91a400e8ca5a6d02976c220b8a0fad183b77
Samuel12209 <samuelbenitez12290@gmail.com>
2024-11-05 12:31:06 -0500

    Init

--- llama2 LM Discord bot.py
+++ llama2 LM Discord bot.py
@@ -3,39 +3,34 @@ from discord.ext import commands
 import requests
 import json
 
-TOKEN = 'discord token'
-API_URL = 'http://localhost:12345/api/generate'
+TOKEN = 'You Discord Bot token' # Bot token!
+API_URL = 'http://localhost:12345/api/generate'  # Removed trailing slash
 
 intents = discord.Intents.default()
-intents.messages = True
 intents.message_content = True
 
-client = commands.Bot(command_prefix='/', intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'Logged in as {client.user}')
+    print(f'Logged in as {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if isinstance(message.channel, discord.DMChannel):
-        await message.channel.send("**Direct Messages not allowed**")
+@bot.command()
+async def ask(ctx, *, prompt):
+    if isinstance(ctx.channel, discord.DMChannel):
+        await ctx.send("**Direct Messages not allowed**")
         return
     
-    if message.content.startswith('/ask'):
-        prompt = message.content.split('/ask ', 1)[1]
-        response = generate_response(prompt)
-        if isinstance(response, tuple):
-            await message.channel.send(response[0])
-            await message.channel.send(response[1])
-        else:
-            await message.channel.send(response)
+    response = generate_response(prompt)
+    if isinstance(response, tuple):
+        await ctx.send(response[0])
+        await ctx.send(response[1])
+    else:
+        await ctx.send(response)
 
-    if message.content.startswith('/test'):
-        await message.channel.send('On')
+@bot.command()
+async def test(ctx):
+    await ctx.send('On')
 
 def generate_response(prompt):
     data = {
@@ -44,35 +39,19 @@ def generate_response(prompt):
     }
     try:
         response = requests.post(API_URL, json=data)
-        if response.status_code == 200:
-            responses = response.content.decode().split('\n')
-            final_response = ''
-            prev_char = ''
-            for resp in responses:
-                if resp.strip():
-                    resp_json = json.loads(resp)
-                    if 'response' in resp_json:
-                        response_text = resp_json['response']
-                        adjusted_text = ''
-                        for char in response_text:
-                            if char == ' ' and prev_char == ' ':
-                                continue
-                            if char in ',.!?':
-                                if prev_char == ' ':
-                                    adjusted_text = adjusted_text[:-1]
-                            adjusted_text += char
-                            prev_char = char
-                        final_response += adjusted_text
-                        
-            if len(final_response) > 1990:
-                cutoff_response = final_response[:1990].strip()
-                remaining_response = final_response[1990:].strip()
-                return cutoff_response, remaining_response
-            else:
-                return final_response.strip()
+        response.raise_for_status()
+        responses = response.json()
+        final_response = ''
+        for resp in responses:
+            if 'response' in resp:
+                final_response += resp['response'].strip() + ' '
+        
+        final_response = final_response.strip()
+        if len(final_response) > 1990:
+            return final_response[:1990], final_response[1990:]
         else:
-            return f"Error generating response: {response.status_code}"
-    except Exception as e:
+            return final_response
+    except requests.RequestException as e:
         return f"Error generating response: {e}"
 
-client.run(TOKEN)
+bot.run(TOKEN)
